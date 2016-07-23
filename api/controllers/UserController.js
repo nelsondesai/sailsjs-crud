9dfdc814cd072cb39396ee537e908dfdb59783ef
Nelson Desai <nelson.desai@gmail.com>
2016-07-23 16:58:26 +0530

    - Added custom signin method in controller - Added Passport middleware

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,8 +4,18 @@
  * @description :: Server-side logic for managing users
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
+var passport = require('passport');
 module.exports = {
-	
+	signin: function (req, res) {
+        passport.authenticate('local', function(err, user, info){
+            if((err) || (!user))
+            {
+                res.status(403);
+                res.send({ message:info.message });
+            }
+            req.session.authenticated =true;
+            return res.ok();
+        })(req, res);
+    }
 };
 
